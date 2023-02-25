2f75cb67aedd75469c67c980501048084ffe30f2
Suyog Shrestha <suyogshrestha97@gmail.com>
2023-02-25 11:11:38 +0545

    changes

---
+++ src/components/videosSection/VideosSection.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+import "./videosSection.scss";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import { PlayIcon } from "../../pages/details/Playbtn";
+import VideoPopup from "../videoPopup/VideoPopup";
+import Img from "../lazyLoad/Img";
+
+const VideosSection = ({ data, loading }) => {
+    const [show, setShow] = useState(false);
+    const [videoId, setVideoId] = useState(null);
+
+    const loadingSkeleton = () => {
+        return (
+            <div className="skItem">
+                <div className="thumb skeleton"></div>
+                <div className="row skeleton"></div>
+                <div className="row2 skeleton"></div>
+            </div>
+        );
+    };
+
+    return (
+        <div className="videosSection">
+            <ContentWrapper>
+                <div className="sectionHeading">Official Videos</div>
+                {!loading ? (
+                    <div className="videos">
+                        Videos data...
+                    </div>
+                ) : (
+                    <div className="videoSkeleton">
+                        {loadingSkeleton()}
+                        {loadingSkeleton()}
+                        {loadingSkeleton()}
+                        {loadingSkeleton()}
+                    </div>
+                )}
+            </ContentWrapper>
+            <VideoPopup
+                show={show}
+                setShow={setShow}
+                videoId={videoId}
+                setVideoId={setVideoId}
+            />
+        </div>
+    );
+};
+
+export default VideosSection;
--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,13 +3,19 @@ import "./details.scss"
 import useFetch from "../../hooks/useFetch";
 import { useParams } from 'react-router-dom';
 import DetailsBanner from './detailsBanner/DetailsBanner';
+import Cast from './cast/Cast';
+import VideosSection from '../../components/videosSection/VideosSection';
+
 
 const Details = () => {
-  // const { mediaType, id } = useParams();
-  // const { data, loading } = useFetch(`/${mediaType}/${id}`)
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`)
+  const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`)
   return (
     <div>
-      <DetailsBanner />
+      <DetailsBanner video={data?.results[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={credits?.loading}/>
+      <VideosSection data={data} loading={loading} />
     </div>
   )
 }
